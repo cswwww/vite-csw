0c76b906e4ae00b861a621bab81a6ac3a6e0bcb5
ReBeX <cswwwx@gmail.com>
2024-10-24 11:52:17 +0800

    style: update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,31 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
 
-export default antfu({
-  ignores: [
-    '**/fixtures',
-    // ...globs
-  ],
-}, {
-  rules: {
-    'no-console': 'off',
-  },
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
 })
+
+export default antfu(
+  {
+    ignores: [
+      '**/fixtures',
+    // ...globs
+    ],
+  },
+  {
+    rules: {
+      'no-console': 'off',
+      'no-alert': 'off',
+      'no-throw-literal': 'off',
+    },
+  },
+
+  // mimic ESLintRC-style extends
+  ...compat.extends('./.eslintrc-auto-import.json'),
+)
